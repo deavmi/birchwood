8e0854e5b57b4fd7dffc0a04b22d9510ff03c404
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-03-08 17:36:05 +0200

    Receiver
    - Added stub class `ReceiverThread`
    
    Sender
    
    - Added stub class `SenderThread`

--- source/birchwood/client/receiver.d
+++ source/birchwood/client/receiver.d
@@ -1,2 +1,12 @@
 module birchwood.client.receiver;
 
+import core.thread : Thread;
+
+// TODO: Examine the below import which seemingly fixes stuff for libsnooze
+import libsnooze.clib;
+import libsnooze;
+
+public final class ReceiverThread : Thread
+{
+
+}
--- source/birchwood/client/sender.d
+++ source/birchwood/client/sender.d
@@ -1,2 +1,12 @@
 module birchwood.client.sender;
 
+import core.thread : Thread;
+
+// TODO: Examine the below import which seemingly fixes stuff for libsnooze
+import libsnooze.clib;
+import libsnooze;
+
+public final class SenderThread : Thread
+{
+
+}
